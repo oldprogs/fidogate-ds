9ac6d148310273f9a591ab2e43ecb2d477ac8c9f
rusfidogate <rusfidogate>
2003-09-24 23:21:24 +0000

    fix version

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 1.2 2003/09/24 02:15:38 rusfidogate Exp $
+ * $Id: version.h,v 1.3 2003/09/24 23:21:24 rusfidogate Exp $
  *
  * Global version number, patch level
  *****************************************************************************/
@@ -10,6 +10,6 @@
 #define VERSION_MAJOR	5
 #define VERSION_MINOR	1
 #define PATCHLEVEL	0
-#define EXTRAVERSION	"-ds"
+#define EXTRAVERSION	"ds-beta1"
 
 #define STATE		"stable"
