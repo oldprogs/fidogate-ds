54205b8b1d6b739a49cbbe1a349d5b67a8f80fe6
rusfidogate <rusfidogate>
2003-12-22 20:58:03 +0000

    See ChangeLog

--- src/common/message.c
+++ src/common/message.c
@@ -2,7 +2,7 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: message.c,v 1.2 2003/12/06 10:28:51 rusfidogate Exp $
+ * $Id: message.c,v 1.3 2003/12/22 20:58:03 rusfidogate Exp $
  *
  * Reading and processing FTN text body
  *
@@ -821,8 +821,10 @@ short int pkt_get_body_parse(FILE *fp, MsgBody *body, Node *from, Node *to)
 	    debug(9, "WARNING: no ' * Origin:' line!");
 #ifdef INSERT_ORIGIN
 	    sprintf(buffer, " * Origin: (%s)\r", znfp1(from));
-	    body->origin = strsave(buffer);
+#else
+	    sprintf(buffer, " * Origin: \r");
 #endif
+	    body->origin = strsave(buffer);
 	}
 	if( body->seenby.n == 0 )
 	{
